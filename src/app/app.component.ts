941e8bbe8f2730f3645ba48850a693eae64eb70c
mantuonweb <mantuonweb@hotmail.com>
2020-10-12 15:35:15 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { setTheme } from 'ngx-bootstrap/utils';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { setTheme } from "ngx-bootstrap/utils";
+import { loadLoginsSuccess } from "./login/store/actions/login.actions";
+import { selectLoginStatus } from "./login/store/selectors/login.selectors";
 
-import { AppState } from './store/reducers';
-import { selectFeatureIsLoggedIn } from './store/selectors';
+import { AppState } from "./store/reducers";
+import { selectFeatureIsLoggedIn } from "./store/selectors";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'online-courses';
+  title = "online-courses";
   isLoggedIn$;
-  constructor(private store: Store<AppState>, private router:Router) { 
+  constructor(private store: Store<AppState>, private router: Router) {
     setTheme("bs4");
     this.isLoggedIn$ = this.store.select(selectFeatureIsLoggedIn);
+    this.checkLogin();
+  }
+  checkLogin() {
+    this.store.select(selectLoginStatus).subscribe(user => {
+      user && sessionStorage.setItem("loggedInStatus", JSON.stringify(user));
+    });
+    setTimeout(() => {
+      
+      let session = sessionStorage.getItem("loggedInStatus");
+      if (session) {
+        let user = JSON.parse(session);
+        this.store.dispatch(loadLoginsSuccess({ user }));
+      }
+    }, 10);
   }
 }
